7a44df19c1b0dc4494b22d2ed9aea525afc33607
Arthur Zhang <zhangwfkl@gmail.com>
2023-01-10 14:29:56 +0800

    Feat: All: Error handling

--- greetings.go
+++ greetings.go
@@ -1,8 +1,12 @@
 package greetings
 
 import "fmt"
+import "errors"
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if (name == "") {
+		return "", errors.New("Empty name");
+	}
 	message := fmt.Sprintf("Hi, %v. Welcom!", name)
-	return message
+	return message, nil;
 }
