dabde2949724e11a4c11396fac95a2ede5918cd3
Arthur Zhang <zhangwfkl@gmail.com>
2023-01-10 15:09:59 +0800

    Feat: All: Use slince

--- greetings.go
+++ greetings.go
@@ -1,12 +1,30 @@
 package greetings
 
-import "fmt"
-import "errors"
+import ("fmt"
+	"errors"
+	"math/rand"
+	"time"
+)
 
 func Hello(name string) (string, error) {
 	if (name == "") {
 		return "", errors.New("Empty name");
 	}
-	message := fmt.Sprintf("Hi, %v. Welcom!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil;
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string{
+	formats := []string {
+		"Hi, %v. Welcome!",
+		"Great to see you, %v",
+		"Hail, %v!. Well met",
+		"High five, %v",
+	};
+
+	return formats[rand.Intn(len(formats))];
 }
